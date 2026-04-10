cf6de37ea41d7cded4539f8fda6396fcb06e97da
Peter <peterviegas@outlook.com>
2026-04-10 13:14:01 +0100

    Created atribute posts in Entity User used anotation @DBRef for reference

--- src/main/java/com/viegas/workshopmongo/config/Instantiation.java
+++ src/main/java/com/viegas/workshopmongo/config/Instantiation.java
@@ -47,6 +47,9 @@ public class Instantiation implements CommandLineRunner {
 		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 		
+		maria.getPost().addAll(Arrays.asList(post1,post2));
+		userRepository.save(maria);
+		
 	}
 
 }
